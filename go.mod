68df15066170433d59448cbbb4f9809dffe4a8aa
钟永标 <zhongyongbiao@lvshou.com>
2021-12-20 17:44:48 +0800

    Add 配置json自定义时间格式输出；

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394
 	github.com/gin-gonic/gin v1.5.0
 	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/json-iterator/go v1.1.12
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/prometheus/client_golang v1.8.0
 	github.com/syyongx/php2go v0.9.4
---
+++ libraries/json/time_format.go
@@ -0,0 +1,217 @@
+package json
+
+import (
+	"github.com/json-iterator/go"
+	"time"
+	"unsafe"
+)
+
+// time format alias
+const (
+	ANSIC       = "ANSIC"
+	UnixDate    = "UnixDate"
+	RubyDate    = "RubyDate"
+	RFC822      = "RFC822"
+	RFC822Z     = "RFC822Z"
+	RFC850      = "RFC850"
+	RFC1123     = "RFC1123"
+	RFC1123Z    = "RFC1123Z"
+	RFC3339     = "RFC3339"
+	RFC3339Nano = "RFC3339Nano"
+	Kitchen     = "Kitchen"
+	Stamp       = "Stamp"
+	StampMilli  = "StampMilli"
+	StampMicro  = "StampMicro"
+	StampNano   = "StampNano"
+	ShangHai    = "ShangHai"
+)
+
+// time zone alias
+const (
+	Local = "Local"
+	UTC   = "UTC"
+)
+
+const (
+	tagNameTimeFormat   = "time_format"
+	tagNameTimeLocation = "time_location"
+)
+
+var CustomTimeFormat = jsoniter.ConfigCompatibleWithStandardLibrary
+
+var formatAlias = map[string]string{
+	ANSIC:       time.ANSIC,
+	UnixDate:    time.UnixDate,
+	RubyDate:    time.RubyDate,
+	RFC822:      time.RFC822,
+	RFC822Z:     time.RFC822Z,
+	RFC850:      time.RFC850,
+	RFC1123:     time.RFC1123,
+	RFC1123Z:    time.RFC1123Z,
+	RFC3339:     time.RFC3339,
+	RFC3339Nano: time.RFC3339Nano,
+	Kitchen:     time.Kitchen,
+	Stamp:       time.Stamp,
+	StampMilli:  time.StampMilli,
+	StampMicro:  time.StampMicro,
+	StampNano:   time.StampNano,
+	ShangHai:    "2006-01-02 15:04:05",
+}
+
+var localeAlias = map[string]*time.Location{
+	Local: time.Local,
+	UTC:   time.UTC,
+}
+
+var (
+	defaultFormat = formatAlias[ShangHai]
+	defaultLocale = time.Local
+)
+
+func init() {
+	CustomTimeFormat.RegisterExtension(&CustomTimeExtension{})
+}
+
+func Marshal(v interface{}) ([]byte, error) {
+	return CustomTimeFormat.Marshal(v)
+}
+
+func Unmarshal(data []byte, v interface{}) error {
+	return CustomTimeFormat.Unmarshal(data, v)
+}
+
+func AddTimeFormatAlias(alias, format string) {
+	formatAlias[alias] = format
+}
+
+func AddLocaleAlias(alias string, locale *time.Location) {
+	localeAlias[alias] = locale
+}
+
+func SetDefaultTimeFormat(timeFormat string, timeLocation *time.Location) {
+	defaultFormat = timeFormat
+	defaultLocale = timeLocation
+}
+
+type CustomTimeExtension struct {
+	jsoniter.DummyExtension
+}
+
+func (extension *CustomTimeExtension) UpdateStructDescriptor(structDescriptor *jsoniter.StructDescriptor) {
+	for _, binding := range structDescriptor.Fields {
+		var typeErr error
+		var isPtr bool
+		typeName := binding.Field.Type().String()
+		if typeName == "time.Time" {
+			isPtr = false
+		} else if typeName == "*time.Time" {
+			isPtr = true
+		} else {
+			continue
+		}
+
+		timeFormat := defaultFormat
+		formatTag := binding.Field.Tag().Get(tagNameTimeFormat)
+		if format, ok := formatAlias[formatTag]; ok {
+			timeFormat = format
+		} else if formatTag != "" {
+			timeFormat = formatTag
+		}
+		locale := defaultLocale
+		if localeTag := binding.Field.Tag().Get(tagNameTimeLocation); localeTag != "" {
+			if loc, ok := localeAlias[localeTag]; ok {
+				locale = loc
+			} else {
+				loc, err := time.LoadLocation(localeTag)
+				if err != nil {
+					typeErr = err
+				} else {
+					locale = loc
+				}
+			}
+		}
+
+		binding.Encoder = &funcEncoder{
+			fun: func(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+				if typeErr != nil {
+					stream.Error = typeErr
+					return
+				}
+
+				var tp *time.Time
+				if isPtr {
+					tpp := (**time.Time)(ptr)
+					tp = *(tpp)
+				} else {
+					tp = (*time.Time)(ptr)
+				}
+
+				if tp != nil {
+					lt := tp.In(locale)
+					str := lt.Format(timeFormat)
+					stream.WriteString(str)
+				} else {
+					stream.WriteNil()
+				}
+			},
+			isEmptyFunc: func(ptr unsafe.Pointer) bool {
+				return (*time.Time)(ptr).IsZero()
+			},
+		}
+
+		binding.Decoder = &funcDecoder{fun: func(ptr unsafe.Pointer, iter *jsoniter.Iterator) {
+			if typeErr != nil {
+				iter.Error = typeErr
+				return
+			}
+
+			str := iter.ReadString()
+			var t *time.Time
+			if str != "" {
+				var err error
+				tmp, err := time.ParseInLocation(timeFormat, str, locale)
+				if err != nil {
+					iter.Error = err
+					return
+				}
+				t = &tmp
+			} else {
+				t = nil
+			}
+
+			if isPtr {
+				tpp := (**time.Time)(ptr)
+				*tpp = t
+			} else {
+				tp := (*time.Time)(ptr)
+				if tp != nil && t != nil {
+					*tp = *t
+				}
+			}
+		}}
+	}
+}
+
+type funcDecoder struct {
+	fun jsoniter.DecoderFunc
+}
+
+func (decoder *funcDecoder) Decode(ptr unsafe.Pointer, iter *jsoniter.Iterator) {
+	decoder.fun(ptr, iter)
+}
+
+type funcEncoder struct {
+	fun         jsoniter.EncoderFunc
+	isEmptyFunc func(ptr unsafe.Pointer) bool
+}
+
+func (encoder *funcEncoder) Encode(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+	encoder.fun(ptr, stream)
+}
+
+func (encoder *funcEncoder) IsEmpty(ptr unsafe.Pointer) bool {
+	if encoder.isEmptyFunc == nil {
+		return false
+	}
+	return encoder.isEmptyFunc(ptr)
+}

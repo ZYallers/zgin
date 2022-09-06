fc2aecf09a66646a3aed45bca35e4be7819017f0
钟永标 <zhongyongbiao@lvshou.com>
2022-09-06 14:28:02 +0800

    Update controller method optimization

--- types/controller.go
+++ types/controller.go
@@ -23,11 +23,10 @@ func (c *Controller) SetContext(ctx *gin.Context) {
 }
 
 func (c *Controller) DumpRequest() string {
-	reqStr, _ := c.Ctx.Get(consts.ReqStrKey)
-	return reqStr.(string)
+	s, _ := c.Ctx.Get(consts.ReqStrKey)
+	return s.(string)
 }
 
-// GetLoggedUserId 获取已登陆用户的ID
 func (c *Controller) GetLoggedUserId() int {
 	if data, ok := c.Ctx.Get(consts.SessDataKey); ok && data != nil {
 		if vars, ok := data.(map[string]interface{}); ok {
@@ -42,8 +41,6 @@ func (c *Controller) GetLoggedUserId() int {
 	return 0
 }
 
-// Json 输出方法
-// args 三个参数: 第一个是code，第二个是msg，第三个是data
 func (c *Controller) Json(a ...interface{}) {
 	var h gin.H
 	switch len(a) {
@@ -58,7 +55,8 @@ func (c *Controller) Json(a ...interface{}) {
 	}
 	bte, err := libJson.Marshal(h)
 	if err != nil {
-		bte = []byte(fmt.Sprintf(`{"code":%d,"msg":"%v"}`, http.StatusInternalServerError, err))
+		s := fmt.Sprintf(`{"code":%d,"msg":"%v","data":{}}`, http.StatusInternalServerError, err)
+		bte = []byte(s)
 	}
 	c.Ctx.Abort()
 	c.Ctx.Status(http.StatusOK)
@@ -66,7 +64,6 @@ func (c *Controller) Json(a ...interface{}) {
 	_, _ = c.Ctx.Writer.Write(bte)
 }
 
-// 从Get和Post里获取Key的值
 func (c *Controller) GetQueryPostForm(keys ...string) string {
 	if len(keys) == 0 {
 		return ""
@@ -83,7 +80,6 @@ func (c *Controller) GetQueryPostForm(keys ...string) string {
 	return ""
 }
 
-// GetQueryByMethod 根据ctx.httpMethod获取GET或POST参数
 func (c *Controller) GetQueryByMethod(key, defaultValue string) string {
 	var query string
 	switch c.Ctx.Request.Method {
@@ -95,7 +91,6 @@ func (c *Controller) GetQueryByMethod(key, defaultValue string) string {
 	return query
 }
 
-// QueryPostNumber 从Query或PostForm中获取数字类型key值
 func (c *Controller) QueryPostNumber(key string, defaultValue ...int) int {
 	if s := c.GetQueryPostForm(key); s != "" {
 		i, _ := strconv.Atoi(s)
@@ -107,7 +102,6 @@ func (c *Controller) QueryPostNumber(key string, defaultValue ...int) int {
 	return 0
 }
 
-// Query 从Get里获取Key的值
 func (c *Controller) Query(keys ...string) string {
 	if len(keys) == 0 {
 		return ""
@@ -121,7 +115,6 @@ func (c *Controller) Query(keys ...string) string {
 	return ""
 }
 
-// PostForm 从Post里获取Key的值
 func (c *Controller) PostForm(keys ...string) string {
 	if len(keys) == 0 {
 		return ""

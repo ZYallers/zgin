c218cd69a66d1f0bd5e95e9577da31f277b0daa1
钟永标 <zhongyongbiao@lvshou.com>
2024-02-27 21:09:17 +0800

    Add opentracing middleware support config exclude routes

--- middleware/tracing.go
+++ middleware/tracing.go
@@ -17,33 +17,61 @@ import (
 
 func WithOpenTracing() option.App {
 	return func(app *types.App) {
-		traceLogDir := conv.ToString(config.AppValue("trace_log_dir"))
-		if traceLogDir == "" {
-			traceLogDir = "/apps/logs/go/trace"
-		}
-		traceLogger := logger.NewLogger(fmt.Sprintf("%s/%s.log", traceLogDir, app.Name))
-		app.Server.Http.Handler.(*gin.Engine).Use(func(ctx *gin.Context) {
-			goId := goid.GetString()
-			defer trace.DelTraceId(goId)
+		logDir := traceLogDir()
+		excludeRoutes := traceExcludeRoutes()
+		traceLogger := logger.NewLogger(fmt.Sprintf("%s/%s.log", logDir, app.Name))
+		app.Server.Http.Handler.(*gin.Engine).Use(traceHandlerFunc(excludeRoutes, traceLogger))
+	}
+}
 
-			traceId := ctx.GetHeader(trace.IdKey)
-			if traceId == "" {
-				traceId = trace.NewTraceId()
+func traceLogDir() string {
+	if v := conv.ToString(config.AppValue("trace_log_dir")); v != "" {
+		return v
+	}
+	return "/apps/logs/go/trace"
+}
+
+func traceExcludeRoutes() map[string]bool {
+	if value := config.AppValue("trace_exclude_routes"); value != nil {
+		if val, ok := value.([]interface{}); ok {
+			if n := len(val); n > 0 {
+				routes := make(map[string]bool, n)
+				for _, s := range val {
+					routes[conv.ToString(s)] = true
+				}
+				return routes
 			}
-			trace.SetTraceId(goId, traceId)
-			ctx.Set(trace.IdKey, traceId)
-			ctx.Header(trace.IdKey, traceId)
+		}
+	}
+	return nil
+}
 
-			traceLogger.Info(app.Name,
-				zap.String("trace_id", traceId),
-				zap.String("host", ctx.Request.Host),
-				zap.String("path", ctx.Request.URL.Path),
-				zap.String("client_ip", ctx.ClientIP()),
-				zap.String("req_raw", ctx.GetString(consts.ReqStrKey)),
-			)
+func traceHandlerFunc(excludeRoutes map[string]bool, traceLogger *zap.Logger) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		if excludeRoutes[ctx.Request.URL.Path[1:]] {
+			return
+		}
 
-			// Must be added ctx.Next(), otherwise there is an issue with the execution order of the defer func above
-			ctx.Next()
-		})
+		goId := goid.GetString()
+		defer trace.DelTraceId(goId)
+
+		traceId := ctx.GetHeader(trace.IdKey)
+		if traceId == "" {
+			traceId = trace.NewTraceId()
+		}
+		trace.SetTraceId(goId, traceId)
+		ctx.Set(trace.IdKey, traceId)
+		ctx.Header(trace.IdKey, traceId)
+
+		traceLogger.Info("",
+			zap.String("trace_id", traceId),
+			zap.String("host", ctx.Request.Host),
+			zap.String("path", ctx.Request.URL.Path),
+			zap.String("client_ip", ctx.ClientIP()),
+			zap.String("req_raw", ctx.GetString(consts.ReqStrKey)),
+		)
+
+		// Must be added ctx.Next(), otherwise there is an issue with the execution order of the defer func above
+		ctx.Next()
 	}
 }
